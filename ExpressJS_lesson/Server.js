2542df7f7d8318ef86d08ba8a3993c26907da3e7
Colt Carnes <Colt.Carnes@BMSGRP.local>
2019-09-18 16:20:34 -0700

    POST routing lesson

--- ExpressJS_lesson/Server.js
+++ ExpressJS_lesson/Server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
 
 // Set our port to listen
 var port = 3000;
@@ -8,7 +9,9 @@ var port = 3000;
 // Serve up our assets for the sites
 // Some Magic going on here, trust it xD
 app.use(express.static("sites/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+// GETs
 // Hello World
 app.get("/", function (req, res) {
 	console.log(req);
@@ -20,9 +23,19 @@ app.get("/drums", function (req, res) {
 	res.sendFile(path.join(__dirname + "/sites/drums/drums.html"));
 });
 
+app.get("/calc", function (req, res) {
+	res.sendFile(path.join(__dirname + "/sites/calculator/calculator.html"));
+});
+
 // Something about me
 app.get("/about", function (req, res) {
 	res.sendFile(path.join(__dirname + "/site/about.html"));
+});
+
+// POSTs
+app.post("/calc", function (req, res) {
+	var total = Number(req.body.num1) + Number(req.body.num2);
+	res.send("Total is : " + total);
 });
 
 // Start our Server
