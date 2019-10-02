d984c74b0f31b616cb0d9fc57672f3b5047b2e74
Colt Carnes <Colt.Carnes@BMSGRP.local>
2019-10-02 16:07:34 -0700

    Add POST method. Add Static Public Assets

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -10,21 +10,28 @@ const app = express();
 const port = 3000;
 
 // App Uses
+app.use(express.static("public")); 
 app.use(bodyParser.urlencoded({extended: true}));
 
 // APIs
 // GET
-app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/signup.html");
 });
 
 
 // POST
+app.post("/", function (req, res) {
+	var firstName = req.body.fName;
+	var lastName = req.body.lName;
+	var email = req.body.email;
 
+	console.log(firstName + " : First Name - " + lastName + " : Last Name - " + email + " : Email");
+});
 // Something here
 
 
 // Listening on 
 app.listen(port, function() {
-	console.log("Server Listening on port : " + port)
+	console.log("Server Listening on port : " + port);
 });
