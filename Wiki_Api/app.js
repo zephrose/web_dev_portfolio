e683bb7ca4b2c39939dc8efccb5265b3779ce79c
Colt Carnes <Colt.Carnes@BMSGRP.local>
2019-11-29 15:01:51 -0800

    Update

--- Wiki_Api/app.js
+++ Wiki_Api/app.js
@@ -33,38 +33,87 @@ const articleSchema = new mongoose.Schema({
 const Article = mongoose.model("Article", articleSchema);
 
 // ROUTES
-app.get("/articles", function(req, res) {
-	Article.find(function(err, foundArticles) {
-		if (!err) {
-			console.log(foundArticles);
-			res.send(foundArticles);
-		} else {
-			console.log(err);
-			res.send(err);
-		}
-	});
-});
-
-app.post("/articles", function(req, res) {
-	if (req.body.title != "") {
-		const newArticle = new Article({
-			title: req.body.title,
-			content: req.body.content
-		});
-		newArticle.save(function(err){
+// ARTICLES
+app.route("/articles")
+	.get(function(req, res) {
+		Article.find(function(err, foundArticles) {
 			if (!err) {
-				console.log("Save Successful");
-				res.status(200).send("Save Successful");
+				console.log(foundArticles);
+				res.send(foundArticles);
 			} else {
-				console.log("Save unsuccessful : ")
-				res.status(500).send("Error attempting to Save Content : " + err);
+				console.log(err);
+				res.send(err);
 			}
-		});	
-	} else {
-		console.log("No title in body. Review contents of body and try again.");
-		res.status(400).send("No title in body. Review contents of body and try again.");
-	}	
-});
+		})
+	})
+	.post(function(req, res) {
+		if (req.body.title != "") {
+			const newArticle = new Article({
+				title: req.body.title,
+				content: req.body.content
+			});
+			newArticle.save(function(err){
+				if (!err) {
+					console.log("Save Successful");
+					res.status(200).send("Save Successful");
+				} else {
+					console.log("Save unsuccessful : ")
+					res.status(500).send("Error attempting to Save Content : " + err);
+				}
+			});	
+		} else {
+			console.log("No title in body. Review contents of body and try again.");
+			res.status(400).send("No title in body. Review contents of body and try again.");
+		}	
+	})
+	.delete(function(req, res) {
+		Article.deleteMany({}, function(err) {
+			if (!err) {
+				console.log("Delete All Articles");
+				res.status(200).send("All Articles Deleted");
+			} else {
+				res.status(500).send("Something went wrong with the deletion. Please check and try again : " + err);
+			}
+		});
+	});
+
+// ARTICLES/:PARAM
+app.route("/articles/:articleTitle")
+	.get(function(req, res) {
+		Article.find({ title: req.params.articleTitle }, null, function(err, foundArticle) {
+			if (!err) {
+				console.log(foundArticle);
+				res.status(200).send(foundArticle);
+			} else {
+				console.log(err);
+				res.status(400).send(err);
+			}
+		});
+	})
+	.put(function(req, res) {
+		Article.updateOne({ title: req.params.articleTitle }, 
+					   { title: req.body.title, content: req.body.content },
+					   function(err, result){
+					   	if (!err) {
+					   		console.log(result);
+					   		res.send("number of documents updated : " + result.n);
+					   	} else {
+					   		console.log(err);
+					   	}
+		});
+	})
+	.patch(function(req, res) {
+		Article.updateOne({ title: req.params.articleTitle }, 
+					   { content: req.body.content },
+					   function(err, result){
+					   	if (!err) {
+					   		console.log(result);
+					   		res.send("number of documents updated : " + result.n);
+					   	} else {
+					   		console.log(err);
+					   	}
+		});
+	});
 
 app.listen(port, function() {
   console.log("Server started on port " + port);
